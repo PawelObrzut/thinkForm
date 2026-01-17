552cf5fbc4e011fb2c849ee290e7b281c007c73d
Pawel Obrzut <pawelobrzut@mac.home>
2026-01-17 11:22:35 +0100

    adds RangeSlider to the Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import RangeSlider from "./components/RangeSlider"
 import TextField from "./components/TextField"
 
 function App() {
   return (
-    <main className='bg-lavander-50 h-screen py-24 px-6 text-deepIndigo'>
-      <form>
+    <main className='min-h-screen bg-lavander-50 py-24 px-6 text-deepIndigo'>
+      <form className='mx-auto w-full max-w-[426px]'>
         <h1 className='text-2xl font-medium mb-8'>
           Personal Info
         </h1>
@@ -12,7 +13,11 @@ function App() {
           <TextField label={'First Name'} name={'firstName'} />
           <TextField label={'Last Name'} name={'lastName'} />
           <TextField label={'Email Address'} name={'email'} type={'email'} />
+
+          <RangeSlider min={8} max={100} label={'Age'} />
+
         </div>
+
 
       </form>
     </main>
---
+++ src/components/RangeSlider.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+type Props = {
+	label: string;
+	min: number;
+	max: number;
+}
+
+const RangeSlider = ({label, min, max}: Props) => {
+  const [rangeValue, setRangeValue] = useState(min);
+  const percentage = ((rangeValue - min) / (max - min)) * 100;
+
+  return (
+    <div className='w-full'>
+      <label className='block'>{label}</label>
+
+      <div className='flex justify-between translate-y-2'>
+        <span>{min}</span>
+        <span>{max}</span>
+      </div>
+
+      <div className='relative'>
+        <input
+          type='range'
+					step='1'
+          min={min}
+          max={max}
+          value={rangeValue}
+          onChange={(e) => setRangeValue(Number(e.target.value))}
+          className='range-slider'
+          style={{ "--progress": `${percentage}%` } as React.CSSProperties}
+        />
+
+        <div className='absolute -translate-x-1/2' style={{ left: `${percentage + 1}%` }} >
+          <div className='absolute z-10 w-2 h-2 -translate-x-1/2 left-1/2 translate-y-1/2 top-[0.25px] bg-white border-l border-t border-lavander-100 rotate-45' />
+          <div className='relative top-2 px-4 py-1 bg-white text-active-100 border border-lavander-100 rounded font-medium'>
+            {rangeValue}
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default RangeSlider
